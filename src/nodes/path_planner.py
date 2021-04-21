ca32162e051af399af993474efdec02ebd9610b9
alindberg999 <alindberg@wpi.edu>
2021-04-21 13:30:19 -0700

    #15 Neighbors of 8 is complete
    using the neighbors of 4 method to add availble spaces, the other 4 corner spaces were checked if availible

--- src/nodes/path_planner.py
+++ src/nodes/path_planner.py
@@ -173,32 +173,33 @@ class PathPlanner:
         #If x is not the value next to the boarder
         if (x!=mapdata.info.width-1):
             #Check is cell is walkable
-            if (self.is_cell_walkable(mapdata, x+1, y)):
+            if (PathPlanner.is_cell_walkable(mapdata, x+1, y)):
                 #If cell can be reached, add it to the list of avaible spaces
                 availibleSpaces.append(x+1,y)
 
         #If the x val is not the 0 boundary
         if (x!=0):
             #Check is cell is walkable
-            if(self.is_cell_walkable(mapdata, x-1, y)):
+            if(PathPlanner.is_cell_walkable(mapdata, x-1, y)):
                 #If cell can be reached, add it to the list of avaible spaces
                 availibleSpaces.append(x-1,y)
         
         #If y is not the value next to the boarder
         if (y!=mapdata.info.height-1):
             #Check is cell is walkable
-            if (self.is_cell_walkable(mapdata, x, y+1)):
+            if (PathPlanner.is_cell_walkable(mapdata, x, y+1)):
                 #If cell can be reached, add it to the list of avaible spaces
                 availibleSpaces.append(x,y+1)
 
         #If the y val is not the 0 boundary
         if (y!=0):
             #Check is cell is walkable
-            if(self.is_cell_walkable(mapdata, x, y-1)):
+            if(PathPlanner.is_cell_walkable(mapdata, x, y-1)):
                 #If cell can be reached, add it to the list of avaible spaces
                 availibleSpaces.append(x,y-1)
 
-
+        return availibleSpaces
+        
     
     
     @staticmethod
@@ -211,7 +212,28 @@ class PathPlanner:
         :return        [[(int,int)]]   A list of walkable 8-neighbors.
         """
         ### REQUIRED CREDIT
-        pass
+
+        availibleSpaces = PathPlanner.neighbors_of_4(mapdata, x, y)
+
+        
+        if(x!=0 and y!=0):
+            if(PathPlanner.is_cell_walkable(mapdata, x-1,y-1)):
+                availibleSpaces.append(x-1,y-1)
+
+        if(x!=mapdata.info.width-1 and y!=mapdata.info.height-1):
+            if(PathPlanner.is_cell_walkable(mapdata, x+1,y+1)):
+                availibleSpaces.append(x+1,y+1)
+
+        if(x!=mapdata.info.width-1 and y!=0):
+            if(PathPlanner.is_cell_walkable(mapdata, x+1,y-1)):
+                availibleSpaces.append(x+1,y-1)
+
+        if(x!=0 and y!=mapdata.info.height-1):
+            if(PathPlanner.is_cell_walkable(mapdata, x-1,y+1)):
+                availibleSpaces.append(x-1,y+1)
+
+        return availibleSpaces
+
 
     
     
